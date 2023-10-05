289e32b0f3186cc1db12e407195e6219f78bd676
João Oliveira <joaopaulosantosoliveira209@gmail.com>
2023-10-05 15:31:16 -0300

    slider automatico bugado

--- scrips/index.js
+++ scrips/index.js
@@ -2,6 +2,9 @@
 const btn_dark = document.querySelector('#darkmod');
 const themeSytem = localStorage.getItem('themeSystem') || "light"
 
+
+
+
 btn_dark.addEventListener('click', ()=>{
     let oldTheme =  localStorage.getItem('themeSystem') || "light";
     let newTheme = oldTheme == "light" ? "dark" : "light" 
@@ -26,18 +29,13 @@ function defineCurrentTheme(theme) {
   }
 }
 
-// defineCurrentTheme(themeSytem)
-
-
-
-
 
 
 //slide de skils
 
 const controls = document.querySelectorAll('.control');
 const items = document.querySelectorAll('.box__skils');
-let primeiroItem = null;
+let primeiroItem = 0;
 const maxItems = items.length;
 
 // Função para selecionar um item
@@ -50,9 +48,43 @@ function selectItem(index) {
   // Adiciona a classe 'primeiro_skil' ao item selecionado
   items[index].classList.add('primeiro_skil');
 
-
-  
 }
+
+
+// faz slide passar automaticamente
+
+const galeria_skils =document.querySelector('.galeria')
+function nextItem(){
+
+  if(items[primeiroItem].classList.contains('primeiro_skil')){
+    items[primeiroItem].classList.remove('primeiro_skil')
+  }
+  console.log(primeiroItem)
+  primeiroItem++
+  
+  if(primeiroItem > items.length -1 ){
+    primeiroItem = 0
+  }
+
+  //corrigir erro do scroll descer sozinho
+  items[primeiroItem].scrollIntoView({
+    inline: 'center',
+    behavior: 'smooth',
+    block: 'nearest',
+  });
+
+  items[primeiroItem].classList.add('primeiro_skil')
+
+
+}
+
+setInterval(nextItem, 5000)
+
+
+
+
+
+
 
 // Adiciona um ouvinte de eventos de clique a cada item
 items.forEach((item, index) => {
@@ -76,10 +108,16 @@ controls.forEach((control) => {
 
     // Atualiza o índice do item atual com base na direção
     if (isLeft) {
-      primeiroItem = (primeiroItem - 1 + maxItems) % maxItems;
+      primeiroItem = (primeiroItem - 1 + maxItems); //% maxItems;
     } else {
-      primeiroItem = (primeiroItem + 1) % maxItems;
+      primeiroItem = (primeiroItem + 1); // % maxItems;
     }
+
+    if (primeiroItem >= maxItems) {
+      primeiroItem = 0; //% maxItems;
+    } 
+
+    console.log(primeiroItem)
 
     // Seleciona o item atual
     selectItem(primeiroItem);
